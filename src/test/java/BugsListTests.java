d6ba3596eb88d1bab18ea41c536e94ef9e0255b2
Joe Tallett <joe@polskiputer.(none)>
2011-10-26 17:19:35 +0100

    guava, bugs list, and change to regexes

--- src/test/java/BugsListTests.java
+++ src/test/java/BugsListTests.java
@@ -1,22 +1,37 @@
 import org.junit.Test;
 
 import java.io.File;
+import java.util.*;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertSame;
 
 public class BugsListTests {
 
+    BugsList list = new BugsList("src/main/resources/bugs.csv");
+
     @Test
     public void testBugListLoadedFromBugFile() {
-        BugsList list = new BugsList("src/main/resources/bugs.csv");
 
         assertEquals(743, list.bugPatterns.size());
 
-        for(Pattern p : list.bugPatterns.keySet()) {
-            System.out.println(p.matcher("www.google-analytics.com/siteopt.js").matches());
+    }
+    
+    @Test
+    public void testMotherPatternMatchesForKnownTrackingUrls() {
+
+        List<String> urls = Arrays.asList(
+                "hello",
+                "hell"
+        );
+
+        for (String url : urls) {
+            assertTrue(list.motherPattern.matcher(url).matches());
         }
 
     }
+
 }
